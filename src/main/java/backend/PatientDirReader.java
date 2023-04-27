8084b838a657c727caa64a219ccca08983b7f4ea
dhauss <davidkhauss@gmail.com>
2023-04-27 10:23:30 -0400

    EHR reader populates DB, Test counts data

--- src/main/java/backend/PatientDirReader.java
+++ src/main/java/backend/PatientDirReader.java
@@ -2,6 +2,7 @@ package backend;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.io.BufferedReader;
@@ -9,11 +10,32 @@ import java.io.BufferedReader;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.WriteResult;
+
 public class PatientDirReader {
 
 	public static void main(String[] args) {
 		String dirPath = "./EHRs/";		
 		Path dir = Paths.get(dirPath);
+		
+		//open Firestore connection
+		String projectId = "medviz-384013";
+
+		Firestore db = null;
+		FirestoreOptions firestoreOptions = null;
+		try {
+			firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
+			    .setProjectId(projectId)
+			    .setCredentials(GoogleCredentials.getApplicationDefault())
+			    .build();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			db = firestoreOptions.getService();
 		
 		//read in all patient JSONs from EHRs directory, parse info and create patient objects to insert into DB
 		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.json")) {
@@ -50,7 +72,16 @@ public class PatientDirReader {
 				
 				patient = new Patient(given, family, city, state, country, phone);
 				
-				System.out.println(patient);
+				String docName = patient.getfName() + "_" + patient.getlName();
+				ApiFuture<WriteResult> future = db.collection("patients").document(docName).set(patient.getPatientMap());
+				try {
+					System.out.println("Update time : " + future.get().getUpdateTime());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+				
 		   }
 
 		} catch(IOException e) {
