2303e44d184a26ea984ec630a15f2ff7e3ab635c
Fayed Gaya <gayafayed@gmail.com>
2023-05-05 01:29:12 -0400

    Enable multi-threadable, implement server GUI

--- src/main/java/backend/Server.java
+++ src/main/java/backend/Server.java
@@ -1,8 +1,22 @@
 package backend;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,11 +31,40 @@ import com.google.cloud.firestore.Query;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 
-public class Server {
+public class Server extends JFrame implements Runnable{
+	
+	// Server GUI Setup
+	private JTextArea serverLog;
+	private ServerSocket serverSocket;
+	private ArrayList<HandleAClient> clientList = new ArrayList<HandleAClient>();
+	private int clientNumber = 0;
+	
+	// FireStore set up
 	private Firestore db = null;
 	private final String PROJECTID = "medviz-384013";
 
 	public Server() {
+		// Server GUI setup
+		super("Med Viz Server");
+		
+		this.setSize(400, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Create the menu bar
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.addActionListener((e) -> System.exit(0));
+		menu.add(exitItem);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		
+			// Create server log
+		serverLog = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(serverLog);
+		JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
+		serverLog.setEditable(false); // disable user input
+		this.add(scrollPane);
+		
 		//initialize DB connection
 		FirestoreOptions firestoreOptions = null;
 		try {
@@ -33,6 +76,13 @@ public class Server {
 			e.printStackTrace();
 		}
 		this.db = firestoreOptions.getService();
+		
+		// Initialize Window
+		this.serverSocket = null;
+		this.setVisible(true);
+		Thread main = new Thread(this);
+		main.start();
+		
 	}
 
 	public void processRequest(String req) {
@@ -165,21 +215,107 @@ public class Server {
 
 		return queryRes;
 	}
-
-	public static void main(String[] args) {
-		ArrayList<Patient> queryRes = new ArrayList<>();
-		Server server = new Server();
-		Patient dave = new Patient("Becky", "Hauss", "avon", "ct", "usa",
-				"555-555-5555", "1989-07-18", "diabetes"
-				);
-		JSONObject daveJSON = dave.getPatientJSON();
-		server.create(daveJSON);
-		/*
-		queryRes = server.readOneField("DOB", "1980-10", "geq");
-		for(Patient patient: queryRes) {
-			System.out.println(patient.getDOB());
+	
+	class HandleAClient implements Runnable{
+		private Socket socket; // Connected socket
+		private int clientNum;
+		
+		public HandleAClient(Socket socket, int clientNum) {
+			this.socket = socket;
+			this.clientNum = clientNum;
 		}
-		*/
+		
+		public void sendMessage(String message) {
+			try {
+				DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+				outputToClient.writeUTF(message);
+				outputToClient.flush();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				// Create input and output streams
+				DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+//				DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+				
+				while (true) {
+					
+					// Receive message from client
+					String inMessage = clientNum + ":" + inputFromClient.readUTF();
+					System.out.println("Server received message: " + inMessage);
+					
+//					// Broadcast the message
+//					broadcastMessage(inMessage);
+//					messageBroadCast = new DataOutputStream(outputToClient);
+//					messageBroadCast.writeUTF(inMessage);
+					
+//					outputToClient.writeUTF(inMessage);
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		// Starts the server and prints to the log
+		try {
+			this.serverSocket = new ServerSocket(9898); // Create a server socket and attach it to port 9898
+			
+			// Log the server start time
+			serverLog.append("MedViz server started at " + new Date() + '\n');
+			System.out.println("Server started. Listening on port 9898.");
+			
+			// Listens for data from clients
+			while (true) {
+				Socket listenerSocket;
+				listenerSocket = serverSocket.accept(); // Accept pending connections from clients
+				clientNumber++;// Increment client number
+				
+				serverLog.append("\nNew Connection!\nStarting thread for client connection" + clientNumber +" at " + new Date() + '\n');
+				// Log client's host name and IP address
+				InetAddress inetAddress = listenerSocket.getInetAddress();
+				serverLog.append("Client host name: " + inetAddress.getHostName() + "\n");
+				serverLog.append("Client IP: " + inetAddress.getHostAddress() + "\n");
+				
+				// Create and start a new thread for the connection
+				HandleAClient clientHandler = new HandleAClient(listenerSocket, clientNumber);
+				Thread clientThread = new Thread(clientHandler);
+				clientThread.start();
+		        clientList.add(clientHandler);
+				
+			}
+		}
+		catch (IOException failedToStart) {
+			failedToStart.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Server server = new Server();
+		
+//		ArrayList<Patient> queryRes = new ArrayList<>();
+//		Server server = new Server();
+//		Patient dave = new Patient("Fayed", "2", "avon", "ct", "usa",
+//				"555-555-5555", "1989-07-18", "diabetes"
+//				);
+//		JSONObject daveJSON = dave.getPatientJSON();
+//		server.create(daveJSON);
+//
+//		queryRes = server.readOneField("DOB", "1980-10", "geq");
+//		for(Patient patient: queryRes) {
+//			System.out.println(patient.getDOB());
+//		}
+
 	}
 
 }
